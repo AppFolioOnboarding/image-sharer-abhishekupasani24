b6c1bd274519eb4ffc9cf2e499659d59635884b6
Abhishek Upasani <abhishek.upasani@appfolio.com>
2019-10-10 16:48:12 -0700

    au - add 20 unique images to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# rubocop:disable Metrics/LineLength
+Image.create!([{ url: 'https://i.pinimg.com/originals/28/4d/01/284d014be8091c2ac52fa9708163bfd6.jpg' },
+               { url: 'https://zdnet2.cbsistatic.com/hub/i/2019/09/05/7c148e17-3f7e-4166-b755-324799ba0c7a/821e5f436179a5f90978a04703fb06ad/atanas-malamov-tpmav6c33de-unsplash.jpg' },
+               { url: 'https://media.wired.com/photos/5c1ae77ae91b067f6d57dec0/master/pass/Comparison-City-MAIN-ART.jpg' },
+               { url: 'https://media.istockphoto.com/photos/abstract-geometric-landscape-picture-id832632796?k=6&m=832632796&s=612x612&w=0&h=fA_-At_kccr38c3MTh_029UbGGpkHSVf-hCULZM3YDw=' },
+               { url: 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg' },
+               { url: 'https://sample-videos.com/img/Sample-jpg-image-500kb.jpg' },
+               { url: 'https://static.toiimg.com/photo/msid-67868104/67868104.jpg?1368689' },
+               { url: 'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg' },
+               { url: 'https://www.tonyseel.com/wp-content/uploads/2019/04/water-image.jpg' },
+               { url: 'https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg' },
+               { url: 'https://cdn.cnn.com/cnnnext/dam/assets/181215042152-nasa-juno-01-large-169.jpg' },
+               { url: 'https://i.all3dp.com/cdn-cgi/image/fit=cover,w=1284,h=722,gravity=0.5x0.5,format=auto/wp-content/uploads/2018/12/28144052/background-images-can-come-in-handy-when-modeling-tian-ooi-all3dp-181228.jpg' },
+               { url: 'https://www.whatsappprofiledpimages.com/wp-content/uploads/2019/01/d3kqway-c2442b67-aa9f-4840--300x266.jpg' },
+               { url: 'https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/48181678987_dd26a6ed67_k.jpg' },
+               { url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_FtwC1LJ6_Q7MkeKmDXB36Oh1VjVUfBpRYq91v09kUaQOYKhi' },
+               { url: 'https://cdn.hasselblad.com/ec67f4db463750c394c4e720acedf6b506b55b48_x1d-ii-sample-01-web.jpg' },
+               { url: 'https://pbs.twimg.com/media/DNhZHGDVAAA97wk.jpg' },
+               { url: 'https://o.aolcdn.com/images/dims?quality=85&image_uri=https%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fcrop%3D904%252C507%252C0%252C0%26quality%3D85%26format%3Djpg%26resize%3D1600%252C897%26image_uri%3Dhttps%253A%252F%252Fs.yimg.com%252Fos%252Fcreatr-uploaded-images%252F2019-09%252Fa252c700-d091-11e9-98d7-456d0782809a%26client%3Da1acac3e1b3290917d92%26signature%3Dbfd0e993df5297c08f967061614c8160d0d9ccd8&client=amp-blogside-v2&signature=5cf955b9a3304615eae2ed4df51146dca86f2421' },
+               { url: 'http://3v6x691yvn532gp2411ezrib-wpengine.netdna-ssl.com/wp-content/uploads/2019/05/imagetext01.jpg' },
+               { url: 'https://webcdn-adespressoinc.netdna-ssl.com/wp-content/uploads/2014/07/new-old-stock_tumblr_n75yr5NJ5L1sfie3io1_1280-1024x698.jpg' }])
+# rubocop:enable Metrics/LineLength
