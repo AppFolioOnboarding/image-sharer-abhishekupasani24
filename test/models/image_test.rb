caf067726bc687cbcdc63ae38c654405fb581ec3
Mike Mullin <mullin7@gmail.com>
2019-10-10 10:08:57 -0700

    AU - Add image model and migration

---
+++ test/models/image_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ImageTest < ActiveSupport::TestCase
+  test 'valid image' do
+    image = Image.new(url: 'https://i.pinimg.com/originals/28/4d/01/284d014be8091c2ac52fa9708163bfd6.jpg')
+    assert_predicate image, :valid?
+  end
+
+  test 'image invalid when no url' do
+    image = Image.new
+    refute_predicate image, :valid?
+  end
+
+  test 'image invalid when incorrect format' do
+    image = Image.new(url: 'http://i.pinimg.com/originals/28/4d/01/284d014be8091c2ac52fa9708163bfd6.jpeg')
+    refute_predicate image, :valid?
+  end
+end
